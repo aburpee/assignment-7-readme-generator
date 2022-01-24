57d3fdaff83c97ae3152f967463b3deb4dd92ada
bonobo <arburpee@gmail.com>
2022-01-23 18:57:07 -0800

    fixed badge and license link

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,32 +2,35 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   const licenseType = license;
-  let licenseString = ''
+  let licenseStringBadge = ''
   if (licenseType == 'MIT') {
-    licenseString = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]'
-  }else if (licenseType == 'GNU GPLv3') {
-    licenseString = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]'
-  }else  if (licenseType == 'Apache License 2.0') {
-    licenseString = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]'
+    licenseStringBadge = '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)'
+  } 
+  if (licenseType == 'GNU GPLv3') {
+    licenseStringBadge = '[License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)'
   }
-  return licenseString;
+  if (licenseType == 'Apache License 2.0') {
+    licenseStringBadge = '[License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)'
+  }
+  return licenseStringBadge;
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-// function renderLicenseLink(license) {
-//   const licenseLink = license.license[0];
-//   let licenseString = ''
-//   if (licenseType = 'MIT') {
-//     licenseString = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]'
-//   };
-//   if (licenseType = 'GNU GPLv3') {
-//     licenseString = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]'
-//   };
-//   if (licenseType = 'Apache License 2.0') {
-//     licenseString = '[![License]](https://opensource.org/licenses/Apache-2.0)'
-//   }
-// }
+function renderLicenseLink(license) {
+  const licenseLink = license;
+  let licenseStringLink = ''
+  if (licenseType = 'MIT') {
+    licenseStringLink = '[License: MIT](https://opensource.org/licenses/MIT)'
+  };
+  if (licenseType = 'GNU GPLv3') {
+    licenseStringLink = '[License: GPL v3](https://www.gnu.org/licenses/gpl-3.0)'
+  };
+  if (licenseType = 'Apache License 2.0') {
+    licenseStringLink = '[License: Apache](https://opensource.org/licenses/Apache-2.0)'
+  }
+  return licenseStringLink;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -36,7 +39,8 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   console.log(data, "Fun1");
-  let licenseLink = renderLicenseBadge(data.license)
+  let licenseBadge = renderLicenseBadge(data.license)
+  let licenseType = renderLicenseLink(data.license)
   const readmeContent = `
   # ${data.title}
   ## Table of Contents
@@ -51,7 +55,9 @@ function generateMarkdown(data) {
    
 
   ### License
-  ${licenseLink}
+  ${licenseBadge}
+
+  ${licenseType}
 
   ### Installation
   ${data.installation}
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
-  # read me generator
+  # Assignment 7: Professional README.md Generator
   ## Table of Contents
   * [License](#license)
   * [Installation](#installation)
@@ -12,22 +12,24 @@
    
 
   ### License
-  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]
+  ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+
+  [License: Apache](https://opensource.org/licenses/Apache-2.0)
 
   ### Installation
-  install node and inquirer using npm i
+  First, install node.js using sudo apt install nodejs. Then, install inquirer using npm install inquirer.
 
   ### Testing
-  run node index.js
+  The user can test the app by running node index.js in the correct directory, in this case Develop. 
 
   ### Description
-  this is a project i am building to generate a readme file using the command line.
+  This project is meant to streamline the rote process of creating a readme file for any github contributions. This will enable users to answer a prompted list of questions that are scalable for any size project. 
 
   ### Languages
   js,node
 
   ### Contributors
-  sangeetha
+  Adam Burpee
 
   ### Email
   If you have any questions please reach out by email. 
